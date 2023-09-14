5e5bf0c2b514ae7b3d988b651a64c8d878092630
Flavio <flavio.nogueira@partner.bmw.com.br>
2023-09-14 16:08:01 -0300

    First commit

---
+++ api/entity/case.ts
@@ -0,0 +1,18 @@
+export default class lawsuit {
+	private _cnj: string;
+	private _class_suit: string;
+	private _distribution_date: Date;
+	private _phase: number;
+	private _foro: string;
+	private _clients: Client[];
+	private _qualification: string;
+	private _defendants: Defendant[];
+	private _case_cost: number;
+	private _events: Event[];
+	private _vara: string;
+	private _last_moviment: Moviment;
+	private _tasks: Task[];
+	private _responsible: Responsible;
+	private _rite: string;
+
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	let cnj = "5019538-35.2023.8.24.0064"
+</script>
+
+<h1 class="init">Casos e processos</h1>
+<p>{cnj}</p>
+
