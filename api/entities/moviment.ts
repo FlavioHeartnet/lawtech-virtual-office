24cbb21e0451be3ea01d974c9f3b16d28fd2cc39
Flavio <flavionogueirabarros@gmail.com>
2023-09-16 02:03:30 -0300

    create court and moviment

--- api/entities/moviment.ts
+++ api/entities/moviment.ts
@@ -1,1 +1,11 @@
-export default class Moviment{}
+import Court from "./value-objects/court";
+
+export default class Moviment{
+    constructor(
+        public id: number,
+        public date: Date,
+        public description: string,
+        public court: Court,
+    ){}
+
+}
---
+++ api/entities/value-objects/court.ts
@@ -0,0 +1,10 @@
+export default class Court{
+    constructor(
+        public name: string,
+        public acronym: string,
+        public court_id: number,
+        public court_type: string,
+        public court_grade: number,
+    ){}
+
+}
