9d204744c3603409e19820576550a1b13fdac941
Flavio <flavio.nogueira@partner.bmw.com.br>
2023-09-22 16:02:13 -0300

    New type of document

--- src/api/@core/entities/value-objects/legal-documents.ts
+++ src/api/@core/entities/value-objects/legal-documents.ts
@@ -3,7 +3,8 @@ export enum documentType {
 	rg = 2,
 	cnh = 3,
 	certidao_nascimento = 4,
-	certidao_casamento = 5
+	certidao_casamento = 5,
+	cnpj = 6,
 }
 export default class LegalDocuments {
 	constructor(public type: documentType, public document_number: string) {}
