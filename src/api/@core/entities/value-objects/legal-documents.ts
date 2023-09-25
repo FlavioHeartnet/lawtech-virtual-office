84ac1c0ffe5669dc62c985508574345a7e4eb20d
Flavio <flavio.nogueira@partner.bmw.com.br>
2023-09-25 16:23:19 -0300

    Legal documents converter to print the value to JSON

--- src/api/@core/entities/value-objects/legal-documents.ts
+++ src/api/@core/entities/value-objects/legal-documents.ts
@@ -20,4 +20,24 @@ export default class LegalDocuments extends ValueObject{
 	static create(props: createLegalDocumentsProps) {
 		return new LegalDocuments(props.type, props.document_number);
 	}
+
+	toJSON() {
+		return {
+			type: this.printDocumentType(this.type),
+			document_number: this.document_number
+		}
+	}
+
+	printDocumentType(type: documentType){
+		switch(type){
+			case documentType.cpf: return 'CPF';
+			case documentType.rg: return 'RG';
+			case documentType.cnh: return 'CNH';
+			case documentType.certidao_nascimento: return 'Certidão de Nascimento';
+			case documentType.certidao_casamento: return 'Certidão de Casamento';
+			case documentType.cnpj: return 'CNPJ';
+			default: return 'Documento inválido';
+		
+		}
+	}
 }
