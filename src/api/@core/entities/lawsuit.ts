2aabec2a191088c4fcde2b2b528fd3999cefa472
Flavio <flavio.nogueira@partner.bmw.com.br>
2023-09-28 16:24:49 -0300

    lawsuit with addition to clients

--- src/api/@core/entities/lawsuit.ts
+++ src/api/@core/entities/lawsuit.ts
@@ -1,4 +1,4 @@
-import type ClassSuit from './value-objects/class-suit';
+import type ClassSuit from './value-objects/lawsuit-class';
 import type Client from './client/client';
 import type Defendant from './defendant';
 import type Event from './event';
@@ -11,6 +11,22 @@ import Uuuid from './value-objects/uuid.vo';
 
 export type CreateLawsuitProps = {
 	cnj: string;
+	subject?: string,
+	class_suit?: ClassSuit,
+	distribution_date?: Date,
+	phase?: Phase,
+	foro?: string,
+	vara?: string,
+	clients?: Client[],
+	qualification?: Qualification,
+	defendants?: Defendant[],
+	case_cost?: number,
+	events?: Event[],
+	last_moviment?: Moviment,
+	tasks?: Task[],
+	responsible?: User,
+	rite?: string,
+	lawsuit_official_link?: string,
 };
 
 export default class lawsuit {
@@ -46,5 +62,40 @@ export default class lawsuit {
 
 	get cnj(): string {
 		return this._cnj;
+	}
+
+	addClient(client: Client){
+		this._clients?.push(client)
+	}
+
+	addTask(task: Task){
+		this._tasks?.push(task)
+	}
+	addEvent(event: Event){
+		this._events?.push(event)
+	}
+
+	toJSON(){
+		return {
+			lawsuit_id: this._lawsuit_id.id,
+			cnj: this._cnj,
+			subject: this._subject,
+			class_suit: this._class_suit,
+			distribution_date: this._distribution_date,
+			phase: this._phase,
+			foro: this._foro,
+			vara: this._vara,
+			clients: this._clients,
+			qualification: this._qualification,
+			defendants: this._defendants,
+			case_cost: this._case_cost,
+			events: this._events,
+			last_moviment: this._last_moviment,
+			tasks: this._tasks,
+			responsible: this._responsible,
+			rite: this._rite,
+			lawsuit_official_link: this._lawsuit_official_link,
+		};
+	
 	}
 }
