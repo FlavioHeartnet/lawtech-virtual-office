a6fcd8efff6aa6a196b57a1e18a8b24eba5d71ab
Flavio <flavionogueirabarros@gmail.com>
2023-10-12 14:49:53 -0300

    creating use case for find by id

--- src/api/@core/usecases/clients/find-client-by-id.uc.ts
+++ src/api/@core/usecases/clients/find-client-by-id.uc.ts
@@ -1,4 +1,7 @@
+import type { CreateAddressDTO } from '../../../dto/address.dto';
 import type { FindByIdDTO, FindByIdOutput } from '../../../dto/client.dtos';
+import type { CreateLegalDocumentsDto } from '../../../dto/legal-documents.dto';
+import Uuuid from '../../entities/value-objects/uuid.vo';
 import { ClientMongoRepository } from '../../infra/mongodb/client/client-mongo.repository';
 import type { IUseCase } from '../use-cases.interface';
 
@@ -6,7 +9,38 @@ export class FindClientbyIdUsecase implements IUseCase<FindByIdDTO, FindByIdOutp
 	constructor(
 		private readonly clientRepository: ClientMongoRepository = new ClientMongoRepository()
 	) {}
-	execute(input: FindByIdDTO): Promise<FindByIdOutput> {
-		throw new Error('Method not implemented.');
+	async execute(input: FindByIdDTO): Promise<FindByIdOutput> {
+		const foundClient =(await this.clientRepository.findById(new Uuuid(input.id))).toJSON();
+        const adresses: CreateAddressDTO[] = foundClient.addresses.map((address) => {
+            return {
+                street: address.street,
+                number: address.address_number,
+                complement: address.complement,
+                city: address.city,
+                state: address.state,
+                country: address.country,
+                description: address.description,
+                zipCode: address.zip,
+            };
+        });
+
+        const legalDocuments: CreateLegalDocumentsDto[] = foundClient.legal_documents.map((legal_document) => {
+            return {
+                type: legal_document.type,
+                document: legal_document.document_number,
+            }; 
+        
+        })
+        return {
+            id: foundClient.client_id,
+            name: foundClient.name,
+            email: foundClient.email,
+            legal_documents: legalDocuments,
+            phone: foundClient.phone,
+            addresses: adresses ,
+            marital_status: foundClient.marital_status,
+            nacionality: foundClient.nacionality,
+            job_title: foundClient.job_title,
+        }
 	}
 }
