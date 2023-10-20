5d75675196f1634eb44615defce941c3a981e7be
Flavio Nogueira <flavionogueirabarros@gmail.com>
2023-10-20 10:47:21 -0300

    repository for client

--- src/api/@core/infra/mongodb/defendant/defendant.repository.ts
+++ src/api/@core/infra/mongodb/defendant/defendant.repository.ts
@@ -1,6 +1,7 @@
 import { MongoConnect } from "../mongo.config";
+import { IDefendantRepository } from "../../../usecases/defendants/repository/defendant.repository";
 
-export class DefendantMongoRepository extends MongoConnect {
+export class DefendantMongoRepository extends MongoConnect implements IDefendantRepository {
 	constructor(){
 		super();
 	}
--- src/api/@core/usecases/defendants/repository/defendant.repository.ts
+++ src/api/@core/usecases/defendants/repository/defendant.repository.ts
@@ -1,2 +1,24 @@
-export interface IDefendantRepository {
+import type {
+	ISearchableRepository
+} from '../../../@shared/repository/repository-interface';
+import { SearchParams } from '../../../@shared/repository/search-params';
+import { SearchResult } from '../../../@shared/repository/search-result';
+import type Defendant from '../../../entities/defendant/defendant';
+import type Uuid from '../../../entities/value-objects/uuid.vo';
+
+export type DefendantFilter = string;
+
+export class DefendantSearchParams extends SearchParams<DefendantFilter> {}
+
+export class DefendantSearchResult extends SearchResult<Defendant> {}
+
+export interface IDefendantRepository
+	extends ISearchableRepository<
+		Defendant,	
+		Uuid,
+		DefendantFilter,
+		DefendantSearchParams,
+		DefendantSearchResult
+	> {
+	insertValidate(entity: Defendant): Promise<void>;
 }
