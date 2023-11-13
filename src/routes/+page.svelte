9e6d4416e5ab663aa847d942803ee771a970b61e
Flavio <flavio.nogueira@partner.bmw.com.br>
2023-11-13 12:04:05 -0300

    login page wip 3 responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,26 @@
 </script>
 
 <div class="mx-5 m-0 h-full flex min-h-screen items-center place-content-center">
-	<form method="post">
-		<h1 class="font-bold">Lawtech Partner</h1><br/>
-		<InputField label='E-mail' name='email' placeholder='Digite seu e-mail' bind:value={email}/>
-		<br/>
-		<InputPassword  label='Password' name='password' placeholder='Digite sua senha' bind:value={password}/>
-		<br/>
-		<div class="mb-2 text-right">
-			<a class="text-blue-modernize hover:text-blue-modernize-dark" href="#">Esqueceu a senha?</a>
+	<div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
+		<div>
+			<img src="https://modernize-nextjs.adminmart.com/images/backgrounds/login-bg.svg" alt=""/>
 		</div>
-		<Button buttonTitle='Entrar' funcHandler />
-		<div class="text-right">
-			<p>Novo por aqui? <a class="text-blue-modernize" href="#">Cadastre-se</a></p>
+		<div>
+			<form method="post">
+				<h1 class="font-bold text-2xl leading-7">Bem vindo ao Lawtech Partner</h1><br/>
+				<InputField label='E-mail' name='email' placeholder='Digite seu e-mail' bind:value={email}/>
+				<br/>
+				<InputPassword  label='Password' name='password' placeholder='Digite sua senha' bind:value={password}/>
+				<br/>
+				<div class="mb-2 text-right">
+					<a class="text-blue-modernize hover:text-blue-modernize-dark" href="#">Esqueceu a senha?</a>
+				</div>
+				<Button buttonTitle='Entrar' funcHandler />
+				<div class="text-right">
+					<p>Novo por aqui? <a class="text-blue-modernize" href="#">Cadastre-se</a></p>
+				</div>
+			</form>
 		</div>
-	</form>
+	</div>
 </div>
 
