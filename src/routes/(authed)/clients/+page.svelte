1b863815cca73ad8f774ffd2699f443e8ecd0cad
Flavio <flavionogueirabarros@gmail.com>
2023-12-14 16:14:19 -0300

    Design: creating table design

--- src/routes/(authed)/clients/+page.svelte
+++ src/routes/(authed)/clients/+page.svelte
@@ -1,6 +1,22 @@
-<div class="p-5 bg-slate-200 text-blue-modernize rounded-lg">
+<div class="p-5  text-blue-modernize rounded-lg">
 	<h1 class="text-2xl">Clientes, Credores e Parceiros</h1>
 </div>
-<div class="container">
-	<table class="table" />
+<div class="modernize-table">
+	<table class="table-auto border-collapse " >
+        <thead class="font-bold">
+            <tr >
+                <th>Nome</th>
+                <th>Tipo</th>
+                <th>Nacionalidade</th>
+                <th>Processo</th>
+            </tr>
+            
+        </thead>
+        <tbody>
+            <th>Flavio</th>
+            <th>Fisica</th>
+            <th>Brasileiro</th>
+            <th>5017579-20.2021.4.04.7200</th>
+        </tbody>
+    </table>
 </div>
