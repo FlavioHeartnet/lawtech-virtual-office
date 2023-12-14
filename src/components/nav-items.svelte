4800b843217b7e6d3a8bb9023bd62954c3c414df
Flavio <flavionogueirabarros@gmail.com>
2023-12-14 14:40:00 -0300

    Design: spacing icon and text in the menu options

--- src/components/nav-items.svelte
+++ src/components/nav-items.svelte
@@ -7,7 +7,7 @@
 
 <a href="{href}">
     <div class="flex p-3 rounded-lg hover:bg-slate-200 hover:text-blue-modernize">
-        <div class="flex-1 w-20"><svelte:component this={Icon}/></div>
+        <div class="flex-auto w-10"><svelte:component this={Icon}/></div>
         <div class="flex-auto w-64"><p>{text}</p></div>
     </div>
 </a>
