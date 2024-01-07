208086d6df2b25fd53b1dc41ec3db8bbefdc69ce
Flavio Nogueira <flavionogueirabarros@gmail.com>
2024-01-07 18:40:22 -0300

    Improve animation for sketeleton leadind

--- src/routes/(authed)/clients/tableLoader.svelte
+++ src/routes/(authed)/clients/tableLoader.svelte
@@ -2,34 +2,15 @@
     <tbody>
       <!-- Adicione mais linhas conforme necessário -->
       <tr>
-        <td></td>
-        <td></td>
-        <td></td>
+        <th><div class="bg-slate-200 p-4 rounded"></div></th>
+        <th><div class="bg-slate-200 p-4 rounded"></div></th>
+        <th><div class="bg-slate-200 p-4 rounded"></div></th>
         <!-- Adicione mais colunas conforme necessário -->
       </tr>
     </tbody>
   </table>
 <style>
-.skeleton-loader {
-	border-collapse: collapse;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-	border-spacing: 0px;
-}
-td {
-	height: 50px;
-	background-color: #e0e0e0;
-}
 
-
-.skeleton-loader td:before {
-  content: '\00a0';
-  display: block;
-  height: 100%;
-  width: 100%;
-  box-sizing: border-box;
-  background: linear-gradient(90deg, #e0e0e0 25%, #f5f5f5 50%, #e0e0e0 75%);
-  animation: loading 1.5s infinite;
-}
 
 @keyframes loading {
   0% {
