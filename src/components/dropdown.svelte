ef0c4ac386d580c296149f9b7b5560906c5defd8
Flavio <flavionogueirabarros@gmail.com>
2024-01-18 17:11:16 -0300

    wip 2 dropdown

--- src/components/dropdown.svelte
+++ src/components/dropdown.svelte
@@ -3,7 +3,6 @@
 
 	import { slide } from "svelte/transition";
     import { quintOut } from 'svelte/easing';
-	import { onMount } from "svelte";
     let isOpen = false;
     let inputValues = [];
     let searchInput = '';
@@ -25,25 +24,19 @@
         isOpen = !isOpen;
     }
     const handleDropdownOption = (id) => {
+        console.log(id);
         const clientName = clients.find((client) => client.id = id );
         if(!inputValues.includes(id)){
             inputValues.push(clientName.name);
         }
-        
-        
-        console.log(inputValues);
     }
-
-    onMount(()=>{
-        
-    });
 
 </script>
 <div class="" id="search-select-client">
-    <input bind:value={searchInput} on:click={handleOpen} on:blur={handleOpen} placeholder="Selecione o cliente" class="py-2.5 px-3 w-full border-2 rounded-lg box-border focus:outline-none focus:border-blue-modernize" type="text" name="clients"> 
+    <input bind:value={searchInput} on:click={handleOpen} on:ended={handleOpen} placeholder="Selecione o cliente" class="py-2.5 px-3 w-full border-2 rounded-lg box-border focus:outline-none focus:border-blue-modernize" type="text" name="clients"> 
     <div class="flex">
         {#each inputValues as client }
-            <div class="flex-auto">{client}</div>  
+            <div class="flex-auto"><p>{client}</p></div>  
         {/each}
     </div>
     
@@ -52,9 +45,9 @@
             {#each clients as client }
                 <!-- svelte-ignore a11y-click-events-have-key-events -->
                 <!-- svelte-ignore a11y-no-static-element-interactions -->
-                <div on:click={handleDropdownOption(client.id)}
+                <a href="#" on:click={handleDropdownOption(client.id)}
                     class="hover:bg-slate-200 hover:rounded p-2">{client.name}
-                </div>
+                </a>
             {/each}
         </div>
     {/if}
