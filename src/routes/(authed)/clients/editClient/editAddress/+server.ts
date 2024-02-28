379d0656a44de181518e8492f38b9a0bd0bb7494
Flavio <flavionogueirabarros@gmail.com>
2024-02-28 16:48:32 -0300

    update comments

--- src/routes/(authed)/clients/editClient/editAddress/+server.ts
+++ src/routes/(authed)/clients/editClient/editAddress/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 import { ClientController } from '../../../../../api/controllers/client.controller.js';
 
-//TODO: Create the use case to update only the client address.
+//TODO: turn this into a +page.server.ts and use Form actions
 export async function POST({ request }) {
 	const data = await request.json();
 	const isUpdate = await new ClientController().updateAddress(data);
